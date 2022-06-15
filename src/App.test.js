ce3e95c07d5084ade6769af94ead44d48bcc3786
Viet Tran <vtran16@bellsouth.net>
2022-06-15 04:53:28 -0500

    Fixed render tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,27 +1,30 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import {AddPost} from "./components/thread/addPost/AddPost";
 
 test('renders login component', () => {
+  const dispatch = jest.fn();
   const _Login = () => <div>MOCK</div>
-  render(<App _Login={_Login}/>);
+  render(<App _Login={_Login} _useDispatch={() => dispatch} _useSelector={() => {}}/>);
   expect(screen.getByText('MOCK')).toBeInTheDocument();
 });
 
 test('renders user component', () => {
+  const dispatch = jest.fn();
   const _User = () => <div>anadmin</div>
-  render(<App _User={_User}/>);
+  render(<App _User={_User} _useDispatch={() => dispatch} _useSelector={() => {}}/>);
   expect(screen.getByText('anadmin')).toBeInTheDocument();
 });
 
 test('renders threadlist component', () => {
+  const dispatch = jest.fn();
   const _ThreadList = () => <div>post</div>
-  render(<App _ThreadList={_ThreadList}/>);
+  render(<App _ThreadList={_ThreadList} _useDispatch={() => dispatch} _useSelector={() => {}}/>);
   expect(screen.getByText('post')).toBeInTheDocument();
 });
 
 test('renders addpost component', () => {
+  const dispatch = jest.fn();
   const _AddPost = () => <div>the ads</div>
-  render(<App _AddPost={_AddPost}/>);
+  render(<App _AddPost={_AddPost} _useDispatch={() => dispatch} _useSelector={() => {}}/>);
   expect(screen.getByText('the ads')).toBeInTheDocument();
 });
